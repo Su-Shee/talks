8155ff6e6b558188d69685761427bcc622e57215
Susanne Schmidt <sushee@outlook.com>
2015-06-18 12:44:04 +0200

    blaerks

--- postgres-meetup-2015/pgtap/traits-data.sql
+++
@@ -1,8 +0,0 @@
--- psql -U postgres -1f traits-data.sql kittycats
-
-INSERT INTO traits (kitty, noise, communication, play, ability)
-  VALUES (1, 'quiet', 'attentive', 'lively', 'nothing special'),
-         (2, 'very quiet', 'distant', 'moderate', 'looks nice'),
-         (3, 'talkative', 'lots', 'active', 'fetches stuff'),
-         (4, 'more quiet', 'very few', 'a little', 'survives cold');
-
